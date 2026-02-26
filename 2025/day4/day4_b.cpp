152886531c3d1a9601242414697818a36f89f06e
Tegan Jain <teganjain527@gmail.com>
2026-02-26 19:24:43 +0530

    chore: separate out 4a from 4b

--- 2025/day4/day4_b.cpp
+++ 2025/day4/day4_b.cpp
@@ -21,13 +21,23 @@ void update_queue(vector<vector<int>> &q, const string &row, const int &idx) {
       }
     }
   }
+  // for (auto e : q[idx - 1])
+  //   cout << e;
+  // cout << "\n";
 }
 
 int check_row(const vector<int> &line, string &prev_row) {
+  // sanity check
+  if (line.size() != prev_row.length()) {
+    cerr << "error: row length not equal to input length\n";
+    exit(1);
+  }
+
   int count{0};
   for (size_t i = 0; i < line.size(); i++) {
     if (prev_row[i] == '@' && line[i] < 4) {
       count++;
+      prev_row[i] = '.';
     }
   }
   return count;
@@ -43,25 +53,29 @@ int main() {
     grid.push_back(std::move(row));
   }
 
+  long long count{0};
   long long result{0};
-  int column{1};
-  vector<int> v(grid[0].length(), 0);
-  // init q to 3 empty rows
-  vector<vector<int>> q(3, v);
+  do {
+    result = 0;
+    int column{1};
+    vector<int> v(grid[0].length(), 0);
+    vector<vector<int>> q(3, v);
 
-  update_queue(q, grid[0], column);
-  // pop unnecessary top row
-  q.erase(q.begin());
-  q.emplace_back(vector<int>(grid[0].length(), 0));
-
-  for (size_t i = 1; i < grid.size(); i++) {
-    update_queue(q, grid[i], column);
-    result += check_row(q[column - 1], grid[i - 1]);
-    column++;
+    update_queue(q, grid[0], column);
+    // pop unnecessary top row
+    q.erase(q.begin());
     q.emplace_back(vector<int>(grid[0].length(), 0));
-  }
 
-  result += check_row(q[column - 1], grid[grid.size() - 1]);
+    for (size_t i = 1; i < grid.size(); i++) {
+      update_queue(q, grid[i], column);
+      result += check_row(q[column - 1], grid[i - 1]);
+      column++;
+      q.emplace_back(vector<int>(grid[0].length(), 0));
+    }
 
-  cout << result << '\n';
+    result += check_row(q[column - 1], grid[grid.size() - 1]);
+    count += result;
+  } while (result != 0);
+
+  cout << count << '\n';
 }
